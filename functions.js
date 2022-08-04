e77fb1580db44bbef942d1bee68494b31e68e878
James Wasmer <jameswasmer@gmail.com>
2022-08-04 17:47:25 -0400

    Add solution for question 2 in functions.js

--- functions.js
+++ functions.js
@@ -8,21 +8,29 @@ for (var i = 0; i < 3; i++) {
 }
 
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
+function getArgument(input) {
+  console.log(`Here's your argument: ${input}.`);
+}
 
+getArgument(`10`);
 
-// 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
-
+// 3: Write a function that has 3 parameters: a string and two numbers. The
+// String will be the name of a company, and the numbers will represent the
+// minimum and maximum of a pay range for a posted job. The function should
+// print out a sentence that includes the name of the company and the range
+// itself (if the numbers passed in are 90000 and 110000, the pay range is
+// 20000).
 
 // 4: Write a function that satifies the following interaction pattern:
 
-checkStock(4, "Coffee");
+// checkStock(4, "Coffee");
 // => "Coffee is stocked"
 
-checkStock(3, "Tortillas");
-// => "Tortillas - running LOW"
-
-checkStock(0, "Cheese");
-// => "Cheese - OUT of stock!"
-
-checkStock(1, "Salsa");
+// checkStock(3, "Tortillas");
+// // => "Tortillas - running LOW"
+//
+// checkStock(0, "Cheese");
+// // => "Cheese - OUT of stock!"
+//
+// checkStock(1, "Salsa");
 // => "Salsa - running LOW"
